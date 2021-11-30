2f73f30efdd915efd50ef8f1a81ad088884ce0fb
bensandland <benjaminsandland@gmail.com>
2021-11-30 10:00:10 +0100

    started working on Shift endpoints

---
+++ controllers/ShiftController.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\controllers;
+
+use app\core\Application;
+use app\core\Controller;
+use app\core\exception\ForbiddenException;
+use app\core\exception\NotFoundException;
+use app\core\middlewares\AuthMiddleware;
+use app\core\Request;
+use app\models\Shift;
+
+class ShiftController extends Controller
+{
+    public function __construct()
+    {
+        $this->registerMiddleware(new AuthMiddleware(['get', 'post', 'delete']));
+    }
+
+    public function get(Request $request)
+    {
+        
+    }
+
+    public function post(Request $request)
+    {
+
+    }
+
+    public function put(Request $request)
+    {
+
+    }
+
+    public function delete(Request $request)
+    {
+        if ($request->isDelete() === false) return;
+
+        if (!Application::$app->user->isAdmin()) {
+            throw new ForbiddenException;
+        }
+
+        $data = $request->getBody();
+
+        if (!isset($data['id'])) {
+            return "You must specify id of the shift";
+        }
+
+        $shift = Shift::findOne(['id' => intval($data['id'])]);
+
+        if ($shift === false) {
+            throw new NotFoundException;
+        }
+
+        if ($shift->delete()) {
+            return true;
+        }
+
+        return $shift->formatErrors();
+    }
+}
---
+++ models/Shift.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\models;
+
+use app\core\Application;
+use app\core\db\DbModel;
+
+class Shift extends DbModel
+{
+    public int $id            = 0;
+    public int $user_id       = 0;
+    public int $department_id = 0;
+    public string $from       = '';
+    public string $to         = '';
+    public int $break_length  = 0;
+
+    public function tableName(): string
+    {
+        return 'Shifts';
+    }
+
+    public function primaryKey(): string
+    {
+        return 'id';
+    }
+
+    public function rules(): array
+    {
+        return [
+            'user_id' => [self::RULE_REQUIRED] [self::RULE_INT],
+            'department_id' => [self::RULE_REQUIRED], [self::RULE_INT],
+            'from' => [self::RULE_REQUIRED],
+            'to' => [self::RULE_REQUIRED],
+            'break_length' => [self::RULE_REQUIRED]
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return ['user_id', 'department_id', 'from', 'to', 'break_length'];
+    }
+
+    public function properties(): array
+    {
+        return ['id', 'user_id', 'department_id', 'from', 'to', 'break_length'];
+    }
+}
